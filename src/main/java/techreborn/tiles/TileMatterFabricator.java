4919ef175d37cee1c33ff85d555736b8c3d8e1aa
Pelotrio <45769595+Pelotrio@users.noreply.github.com>
2022-01-22 17:34:42 +0100

    Removed performance intensive markDirty

--- src/main/java/techreborn/tiles/TileMatterFabricator.java
+++ src/main/java/techreborn/tiles/TileMatterFabricator.java
@@ -44,178 +44,182 @@ import techreborn.utils.OreDictUtils;
 
 @RebornRegistry(modID = ModInfo.MOD_ID)
 public class TileMatterFabricator extends TilePowerAcceptor
-		implements IToolDrop, IInventoryProvider, IContainerProvider {
+        implements IToolDrop, IInventoryProvider, IContainerProvider {
 
-	@ConfigRegistry(config = "machines", category = "matter_fabricator", key = "MatterFabricatorMaxInput", comment = "Matter Fabricator Max Input (Value in EU)")
-	public static int maxInput = 8192;
-	@ConfigRegistry(config = "machines", category = "matter_fabricator", key = "MatterFabricatorMaxEnergy", comment = "Matter Fabricator Max Energy (Value in EU)")
-	public static int maxEnergy = 10_000_000;
-	@ConfigRegistry(config = "machines", category = "matter_fabricator", key = "MatterFabricatorFabricationRate", comment = "Matter Fabricator Fabrication Rate, amount of amplifier units per UUM")
-	public static int fabricationRate = 6_000;
-	@ConfigRegistry(config = "machines", category = "matter_fabricator", key = "MatterFabricatorEnergyPerAmp", comment = "Matter Fabricator EU per amplifier unit, multiply this with the rate for total EU")
-	public static int energyPerAmp = 5;
+    @ConfigRegistry(config = "machines", category = "matter_fabricator", key = "MatterFabricatorMaxInput", comment = "Matter Fabricator Max Input (Value in EU)")
+    public static int maxInput = 8192;
+    @ConfigRegistry(config = "machines", category = "matter_fabricator", key = "MatterFabricatorMaxEnergy", comment = "Matter Fabricator Max Energy (Value in EU)")
+    public static int maxEnergy = 10_000_000;
+    @ConfigRegistry(config = "machines", category = "matter_fabricator", key = "MatterFabricatorFabricationRate", comment = "Matter Fabricator Fabrication Rate, amount of amplifier units per UUM")
+    public static int fabricationRate = 6_000;
+    @ConfigRegistry(config = "machines", category = "matter_fabricator", key = "MatterFabricatorEnergyPerAmp", comment = "Matter Fabricator EU per amplifier unit, multiply this with the rate for total EU")
+    public static int energyPerAmp = 5;
 
-	public Inventory inventory = new Inventory(12, "TileMatterFabricator", 64, this);
-	private int amplifier = 0;
+    public Inventory inventory = new Inventory(12, "TileMatterFabricator", 64, this);
+    private int amplifier = 0;
 
-	public TileMatterFabricator() {
-		super();
-	}
+    public TileMatterFabricator() {
+        super();
+    }
 
-	private boolean spaceForOutput() {
-		for (int i = 6; i < 11; i++) {
-			if (spaceForOutput(i)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    private boolean spaceForOutput() {
+        for (int i = 6; i < 11; i++) {
+            if (spaceForOutput(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private boolean spaceForOutput(int slot) {
-		return inventory.getStackInSlot(slot).isEmpty()
-				|| ItemUtils.isItemEqual(inventory.getStackInSlot(slot), new ItemStack(ModItems.UU_MATTER), true, true)
-						&& inventory.getStackInSlot(slot).getCount() < 64;
-	}
+    private boolean spaceForOutput(int slot) {
+        return inventory.getStackInSlot(slot).isEmpty()
+                || ItemUtils.isItemEqual(inventory.getStackInSlot(slot), new ItemStack(ModItems.UU_MATTER), true, true)
+                && inventory.getStackInSlot(slot).getCount() < 64;
+    }
 
-	private void addOutputProducts() {
-		for (int i = 6; i < 11; i++) {
-			if (spaceForOutput(i)) {
-				addOutputProducts(i);
-				break;
-			}
-		}
-	}
+    private void addOutputProducts() {
+        for (int i = 6; i < 11; i++) {
+            if (spaceForOutput(i)) {
+                addOutputProducts(i);
+                break;
+            }
+        }
+    }
 
-	private void addOutputProducts(int slot) {
-		ItemStack stack = getStackInSlot(slot);
-		if (stack.isEmpty()) {
-			inventory.setInventorySlotContents(slot, new ItemStack(ModItems.UU_MATTER));
-		} 
-		else if (ItemUtils.isItemEqual(stack, new ItemStack(ModItems.UU_MATTER), true, true) && stack.getCount() < stack.getMaxStackSize()) {
-			inventory.getStackInSlot(slot).grow(1);
-		}
-	}
+    private void addOutputProducts(int slot) {
+        ItemStack stack = getStackInSlot(slot);
+        if (stack.isEmpty()) {
+            inventory.setInventorySlotContents(slot, new ItemStack(ModItems.UU_MATTER));
+        } else if (ItemUtils.isItemEqual(stack, new ItemStack(ModItems.UU_MATTER), true, true) && stack.getCount() < stack.getMaxStackSize()) {
+            inventory.getStackInSlot(slot).grow(1);
+        }
+    }
 
-	public boolean decreaseStoredEnergy(double aEnergy, boolean aIgnoreTooLessEnergy) {
-		if (getEnergy() - aEnergy < 0 && !aIgnoreTooLessEnergy) {
-			return false;
-		} else {
-			setEnergy(getEnergy() - aEnergy);
-			if (getEnergy() < 0) {
-				setEnergy(0);
-				return false;
-			} else {
-				return true;
-			}
-		}
-	}
+    public boolean decreaseStoredEnergy(double aEnergy, boolean aIgnoreTooLessEnergy) {
+        if (getEnergy() - aEnergy < 0 && !aIgnoreTooLessEnergy) {
+            return false;
+        } else {
+            setEnergy(getEnergy() - aEnergy);
+            if (getEnergy() < 0) {
+                setEnergy(0);
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
 
-	public int getValue(ItemStack itemStack) {
-		if (OreDictUtils.isOre(itemStack, "itemScrap") ||
-			OreDictUtils.isOre(itemStack, "materialScrap")) return 200;
+    public int getValue(ItemStack itemStack) {
+        if (OreDictUtils.isOre(itemStack, "itemScrap") ||
+                OreDictUtils.isOre(itemStack, "materialScrap")) return 200;
 
-		if (OreDictUtils.isOre(itemStack, "itemScrapBox") ||
-			OreDictUtils.isOre(itemStack, "materialScrapBox")) return 2000;
+        if (OreDictUtils.isOre(itemStack, "itemScrapBox") ||
+                OreDictUtils.isOre(itemStack, "materialScrapBox")) return 2000;
 
-		return 0;
-	}
+        return 0;
+    }
 
-	public int getProgress() {
-		return amplifier;
-	}
+    public int getProgress() {
+        return amplifier;
+    }
 
-	public void setProgress(int progress) {
-		amplifier = progress;
-	}
+    public void setProgress(int progress) {
+        amplifier = progress;
+    }
 
-	public int getProgressScaled(int scale) {
-		if (amplifier != 0) {
-			return Math.min(amplifier * scale / fabricationRate, 100);
-		}
-		return 0;
-	}
+    public int getProgressScaled(int scale) {
+        if (amplifier != 0) {
+            return Math.min(amplifier * scale / fabricationRate, 100);
+        }
+        return 0;
+    }
 
-	// TilePowerAcceptor
+    // TilePowerAcceptor
+    @Override
+    public void update() {
+        if (world.isRemote) {
+            return;
+        }
+
+        super.update();
+        this.charge(11);
+
+        for (int i = 0; i < 6; i++) {
+            final ItemStack stack = inventory.getStackInSlot(i);
+            if (!stack.isEmpty() && spaceForOutput()) {
+                final int amp = getValue(stack);
+                final int euNeeded = amp * energyPerAmp;
+                if (amp != 0 && this.canUseEnergy(euNeeded)) {
+                    useEnergy(euNeeded);
+                    amplifier += amp;
+                    inventory.decrStackSize(i, 1);
+                }
+            }
+        }
+
+        if (amplifier >= fabricationRate) {
+            if (spaceForOutput()) {
+                addOutputProducts();
+                amplifier -= fabricationRate;
+            }
+        }
+    }
+
+    @Override
+    public double getBaseMaxPower() {
+        return maxEnergy;
+    }
+
+    @Override
+    public boolean canAcceptEnergy(EnumFacing direction) {
+        return true;
+    }
+
+    @Override
+    public boolean canProvideEnergy(EnumFacing direction) {
+        return false;
+    }
+
+    @Override
+    public double getBaseMaxOutput() {
+        return 0;
+    }
+
+    @Override
+    public double getBaseMaxInput() {
+        return maxInput;
+    }
+
+    // RebornMachineTile
+    @Override
+    public boolean canBeUpgraded() {
+        return false;
+    }
+
+    // IToolDrop
+    @Override
+    public ItemStack getToolDrop(EntityPlayer entityPlayer) {
+        return new ItemStack(ModBlocks.MATTER_FABRICATOR, 1);
+    }
+
+    // IInventoryProvider
+    @Override
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    // IContainerProvider
+    @Override
+    public BuiltContainer createContainer(EntityPlayer player) {
+        return new ContainerBuilder("matterfabricator").player(player.inventory).inventory().hotbar().addInventory()
+                .tile(this).slot(0, 30, 20).slot(1, 50, 20).slot(2, 70, 20).slot(3, 90, 20).slot(4, 110, 20)
+                .slot(5, 130, 20).outputSlot(6, 40, 66).outputSlot(7, 60, 66).outputSlot(8, 80, 66)
+                .outputSlot(9, 100, 66).outputSlot(10, 120, 66).energySlot(11, 8, 72).syncEnergyValue()
+                .syncIntegerValue(this::getProgress, this::setProgress).addInventory().create(this);
+    }
+
 	@Override
-	public void update() {
-		if (world.isRemote) {
-			return;
-		}
-
-		super.update();
-		this.charge(11);
-
-		for (int i = 0; i < 6; i++) {
-			final ItemStack stack = inventory.getStackInSlot(i);
-			if (!stack.isEmpty() && spaceForOutput()) {
-				final int amp = getValue(stack);
-				final int euNeeded = amp * energyPerAmp;
-				if (amp != 0 && this.canUseEnergy(euNeeded)) {
-					useEnergy(euNeeded);
-					amplifier += amp;
-					inventory.decrStackSize(i, 1);
-				}
-			}
-		}
-
-		if (amplifier >= fabricationRate) {
-			if (spaceForOutput()) {
-				addOutputProducts();
-				amplifier -= fabricationRate;
-			}
-		}
-	}
-
-	@Override
-	public double getBaseMaxPower() {
-		return maxEnergy;
-	}
-
-	@Override
-	public boolean canAcceptEnergy(EnumFacing direction) {
-		return true;
-	}
-
-	@Override
-	public boolean canProvideEnergy(EnumFacing direction) {
-		return false;
-	}
-
-	@Override
-	public double getBaseMaxOutput() {
-		return 0;
-	}
-
-	@Override
-	public double getBaseMaxInput() {
-		return maxInput;
-	}
-
-	// RebornMachineTile
-	@Override
-	public boolean canBeUpgraded() {
-		return false;
-	}
-
-	// IToolDrop
-	@Override
-	public ItemStack getToolDrop(EntityPlayer entityPlayer) {
-		return new ItemStack(ModBlocks.MATTER_FABRICATOR, 1);
-	}
-
-	// IInventoryProvider
-	@Override
-	public Inventory getInventory() {
-		return inventory;
-	}
-
-	// IContainerProvider
-	@Override
-	public BuiltContainer createContainer(EntityPlayer player) {
-		return new ContainerBuilder("matterfabricator").player(player.inventory).inventory().hotbar().addInventory()
-				.tile(this).slot(0, 30, 20).slot(1, 50, 20).slot(2, 70, 20).slot(3, 90, 20).slot(4, 110, 20)
-				.slot(5, 130, 20).outputSlot(6, 40, 66).outputSlot(7, 60, 66).outputSlot(8, 80, 66)
-				.outputSlot(9, 100, 66).outputSlot(10, 120, 66).energySlot(11, 8, 72).syncEnergyValue()
-				.syncIntegerValue(this::getProgress, this::setProgress).addInventory().create(this);
+	public void markDirty() {
+		this.world.markChunkDirty(this.pos, this);
 	}
 }
